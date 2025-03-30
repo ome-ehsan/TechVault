38c5aeac044b31141260aadaa5d3a036dfa8783d
ome-ehsan <voidcryptic01@gmail.com>
2025-03-30 21:15:10 +0600

    create db connection

--- server/app.js
+++ server/app.js
@@ -1,1 +1,13 @@
-// entry point of server side
+import express from 'express'
+import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser';
+import { connectDB } from './utils/dbConfig.js';
+dotenv.config();
+
+const port = process.env.PORT || 8001;
+const app = express();
+
+app.listen( port, ()=>{
+    console.log("Server started on port", port);
+    connectDB();
+})
---
+++ server/utils/dbConfig.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+dotenv.config()
+
+export const connectDB = async () =>{
+    try{
+        const connection = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`Database connection successfull. Host: ${connection.connection.host}`);
+    }catch(err){
+        console.log("Database error : ", err);
+    }
+};
