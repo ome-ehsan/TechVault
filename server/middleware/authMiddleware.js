a1f290f86dcbb34a4df025a7489799595f5394d0
ome-ehsan <voidcryptic01@gmail.com>
2025-04-01 17:14:43 +0600

    comment out authorize method

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -22,11 +22,11 @@ export const authenticate = async ( req , res , next ) => {
 };
 
 
-export const authorize = (role) => {
-    return (req,res,next) => {
-        if ( !req.user || req.user.role != role ){
-            return res.status(403).json({ msg : "Access Denied"});
-        }
-        next();
-    }
-};
+// export const authorize = (role) => {
+//     return (req,res,next) => {
+//         if ( !req.user || req.user.role != role ){
+//             return res.status(403).json({ msg : "Access Denied"});
+//         }
+//         next();
+//     }
+// };
