22dd5fa0c47fce951391f4ca3594e4ebe1aefaed
ome-ehsan <voidcryptic01@gmail.com>
2025-05-04 12:41:55 +0600

    setup for deployment

--- client/src/components/FilterBar.jsx
+++ client/src/components/FilterBar.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { categoryToSpecMap } from '../../../shared/categoryToSpecMap'
+import { categoryToSpecMap } from '../libs/categoryToSpecMap'
 
 const FilterBar = ({ 
   selectedCategory,
--- client/src/libs/axios.js
+++ client/src/libs/axios.js
@@ -1,5 +1,8 @@
 import axios from "axios"
+
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/api";
+
 export const axiosInstance = axios.create({
-    baseURL :"http://localhost:8000/api",
-    withCredentials : true
+    baseURL: BASE_URL,
+    withCredentials: true
 });
