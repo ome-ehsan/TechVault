14f50db41573d4ad542b79bb1eeddff9c9d55de1
ome-ehsan <voidcryptic01@gmail.com>
2025-05-04 14:22:36 +0600

    fix same site issue_2

--- server/utils/jwtConfig.js
+++ server/utils/jwtConfig.js
@@ -1,14 +1,14 @@
 import jwt from 'jsonwebtoken'
 
-export const generateJWT = (userId,res)=>{
-    const token = jwt.sign({userId}, process.env.SECRET, { expiresIn: "3d"});
-    // send token in cookie
+export const generateJWT = (userId, res) => {
+    const token = jwt.sign({ userId }, process.env.SECRET, { expiresIn: "3d" });
+
     res.cookie("jwt", token, {
-        maxAge : 60*60*24*1000*3,  // 3 day
-        httpOnly : true,
-        sameSite : "None",
-        secure : process.env.NODE_ENV !== "development"  
-    })
+        maxAge: 3 * 24 * 60 * 60 * 1000, // 3 days
+        httpOnly: true,
+        sameSite: process.env.NODE_ENV === "development" ? "Lax" : "None",
+        secure: process.env.NODE_ENV !== "development"
+    });
+
     return token;
-    
-}
+};
