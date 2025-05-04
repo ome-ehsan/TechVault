df7424d52ba605de45e85cec06de5f1bf6eab0c6
ome-ehsan <voidcryptic01@gmail.com>
2025-05-04 14:16:18 +0600

    fix same site issue

--- server/utils/jwtConfig.js
+++ server/utils/jwtConfig.js
@@ -6,7 +6,7 @@ export const generateJWT = (userId,res)=>{
     res.cookie("jwt", token, {
         maxAge : 60*60*24*1000*3,  // 3 day
         httpOnly : true,
-        sameSite : "strict",
+        sameSite : "None",
         secure : process.env.NODE_ENV !== "development"  
     })
     return token;
